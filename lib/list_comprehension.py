adea2c39ee15f8d967627812a880e9907fb3be29
Elly <ellyjames1999@gmail.com>
2025-02-12 23:10:03 +0300

    done all tests

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,30 @@
 #!/usr/bin/env python3
 
+#Using a list comprehension, write a function return_evens() 
+# that returns a list of all of the even elements of a sequence of integers.
+# return_evens([0, 1, 3, 5, 7, 8, 9])
+# [0, 8]
+# Using a list comprehension, write a function make_exclamation()
+#  that takes a list of sentence strings and returns a list of sentence strings with exclamation marks at the end.
+# make_exclamation(["Hello", "I'm doing great", "Python is fun"])
+# ["Hello!", "I'm doing great!", "Python is fun!"]
+
 def return_evens(num_list):
-    pass
+    even_list = [num for num in num_list if num % 2 == 0]
+
+    return even_list
+
+print(return_evens([0, 1, 3, 5, 7, 8, 9]))
+
+
+# Using a list comprehension, write a function make_exclamation() that takes a list of sentence strings and returns a list of sentence strings with exclamation marks at the end
+# make_exclamation(["Hello", "I'm doing great", "Python is fun"])
+# ["Hello!", "I'm doing great!", "Python is fun!"]
 
 def make_exclamation(sentence_list):
-    pass
+    exclamation_list = [sentence + "!" for sentence in sentence_list]
+
+    return exclamation_list
+
+print(make_exclamation(["Hello", "I'm doing great", "Python is fun"]))
+    
